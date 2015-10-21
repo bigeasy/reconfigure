01adf76d6cfc2b5b83e7d9947087ae3c413c92a1
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-21 22:19:37 +0000

    Test in a container.

--- t/reconfigure/wrapper.t.js
+++ t/reconfigure/wrapper.t.js
@@ -1,22 +1,28 @@
-require('proof')(2, require('cadence')(prove))
-
-function prove (async, assert) {
-    var wrapper = require('../../reconfigure/wrapper')
-    var uuid = require('node-uuid')
-    var wrap = new wrapper('127.0.0.1', '4001')
-    var path = uuid.v4()
-    async(function () {
-        wrap.initialize(async())
-        // this creates a `reconfigure` directory which will cause
-        //a 'Not a file' error from `etcd` next time this is run.
-    }, function () {
-        wrap.mkdir(path, async())
-    }, function () {
-        wrap.set('/' + path + '/blegh', 'haha', async())
-    }, function (set) {
-        assert(set.node.key, '/reconfigure/' + path + '/blegh', 'key set')
-        wrap.get('/' + path + '/blegh', async())
-    }, function (key) {
-        assert(key.node.value, 'haha', 'retrieved monitored value')
-    })
-}
+var exec = require('child_process').exec
+exec('docker run -p 127.0.0.1:7001:4001 -p 127.0.0.1:2379:2379 --rm quay.io/coreos/etcd:v2.2.0 --name reconfigure-etcd -name reconfigure-etcd',
+function () {
+    require('proof')(2, require('cadence')(prove))
+    function prove (async, assert) {
+        var wrapper = require('../../reconfigure/wrapper')
+        var wrap = new wrapper('127.0.0.1', '2379')
+        async(function () {
+            wrap.initialize(async())
+            // this creates a `reconfigure` directory which will cause
+            //a 'Not a file' error from `etcd` next time this is run.
+        }, function () {
+            console.log(arguments)
+            wrap.mkdir('/test', async())
+        }, function () {
+            wrap.set('/test/blegh', 'haha', async())
+        }, function (set) {
+            assert(set.node.key, '/reconfigure/test/blegh', 'key set')
+            wrap.get('/test/blegh', async())
+        }, function (key) {
+            assert(key.node.value, 'haha', 'retrieved monitored value')
+            exec('docker kill reconfigure-etcd',
+            function () {console.log(arguments)})
+            exec('docker rm reconfigure-etcd',
+            function () {console.log(arguments)})
+        })
+    }
+})
