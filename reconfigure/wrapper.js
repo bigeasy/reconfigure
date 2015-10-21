7f5f061cf56f25dae592c6064cd6f6714170038c
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-21 19:21:49 +0000

    get, set, mkdir.

--- reconfigure/wrapper.js
+++ reconfigure/wrapper.js
@@ -6,12 +6,12 @@ function Wrapper (host, port) {
 }
 
 Wrapper.prototype.initialize = cadence(function (async) {
-    this.mkdir('/reconfigure', async())
+    this.mkdir('', async())
 })
 
 Wrapper.prototype.set = cadence(function (async, key, val) {
 // flat hierarchy so `val` should always be
-    this._etcd.set('/reconfigure/' + key, val, async())
+    this._etcd.set('/reconfigure' + key, val, async())
 })
 
 Wrapper.prototype.get = cadence(function (async, key) {
@@ -21,7 +21,11 @@ Wrapper.prototype.get = cadence(function (async, key) {
 
 Wrapper.prototype.mkdir = cadence(function (async, dir) {
     this._etcd.mkdir('/reconfigure' + dir, async())
-}
+})
+
+Wrapper.prototype.rmdir = cadence(function (async, dir) {
+    this._etcd.rmdir('/reconfigure' + dir, async())
+})
 
 /*
 Wrapper.prototype.watch = function (key, callback) {
--- t/reconfigure/wrapper.t.js
+++ t/reconfigure/wrapper.t.js
@@ -2,12 +2,20 @@ require('proof')(2, require('cadence')(prove))
 
 function prove (async, assert) {
     var wrapper = require('../../reconfigure/wrapper')
+    var uuid = require('node-uuid')
     var wrap = new wrapper('127.0.0.1', '4001')
+    var path = uuid.v4()
     async(function () {
-        wrap.set('/keys/blah', 'haha', async())
+        wrap.initialize(async())
+        // this creates a `reconfigure` directory which will cause
+        //a 'Not a file' error from `etcd` next time this is run.
+    }, function () {
+        wrap.mkdir(path, async())
+    }, function () {
+        wrap.set('/' + path + '/blegh', 'haha', async())
     }, function (set) {
-        assert(set.node.key, '/keys/blah', 'key set')
-        wrap.get('/keys/blah', async())
+        assert(set.node.key, '/reconfigure/' + path + '/blegh', 'key set')
+        wrap.get('/' + path + '/blegh', async())
     }, function (key) {
         assert(key.node.value, 'haha', 'retrieved monitored value')
     })
