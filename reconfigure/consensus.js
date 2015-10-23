789189aec069bf41e84f17f149e78aa146fbab56
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-22 21:46:47 -0400

    `Watch` and `stop`.

--- reconfigure/consensus.js
+++ reconfigure/consensus.js
@@ -1,8 +1,21 @@
 var Etcd = require('node-etcd')
 var cadence = require('cadence')
+var Delta = require('delta')
+var turnstile = require('turnstile')
+var abend = require('abend')
 
-function Consensus (host, port) {
+function Consensus (host, port, listener) {
     this._etcd = new Etcd(host, port)
+    this._watcher = null
+    this._listener = listener // <- error first callback, if we get an error we panic.
+    this._turnstile = new turnstile.Turnstile
+}
+
+Consensus.prototype.stop = function () {
+    if (this._watcher != null) {
+        this._watcher.stop()
+        console.log('stopped')
+    }
 }
 
 Consensus.prototype.initialize = cadence(function (async) {
@@ -23,8 +36,29 @@ Consensus.prototype.get = cadence(function (async, key) {
   this._etcd.get('/reconfigure/' + key, async())
 })
 
-Consensus.prototype.watch = cadence(function (async, key) {
-    this._etcd.watch('/reconfigure/' + key, async())
+Consensus.prototype._changed = turnstile.throttle(cadence(function (async) {
+    async(function () {
+        this._list('/reconfigure', async()) // <- error -> panic!
+    }, function (object) {
+        this._listener(object, async()) // <- error -> panic!
+        // todo: what if there's a synchronous error? Are we going to stack them
+        // up in the next tick queue?
+        // ^^^ should, we don't know how, use Cadence exceptions to do the right
+        // thing.
+    })
+}))
+
+Consensus.prototype.watch = cadence(function (async) {
+    this._watcher = this._etcd.watcher('/reconfigure', null, { recursive: true })
+    new Delta(async()).ee(this._watcher).on('change', function (whatIsThis) {
+        // ^^^ change
+        console.log(arguments)
+        this._changed(abend)
+    }.bind(this)).on('stop')
 })
+
+function main () {
+    consesus.watch(abend)
+}
 
 module.exports = Consensus
