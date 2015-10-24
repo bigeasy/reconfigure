93409f354242af4e467bfb7b5e463de40cda4c04
Alan Gutierrez <alan@prettyrobots.com>
2015-10-24 14:01:37 -0400

    Notes.

--- reconfigure/consensus.js
+++ reconfigure/consensus.js
@@ -9,6 +9,7 @@ function Consensus (host, port, listener) {
     this._watcher = null
     this._listener = listener // <- error first callback, if we get an error we panic.
     this._turnstile = new turnstile.Turnstile
+    this._properties = {}
 }
 
 Consensus.prototype.stop = function () {
@@ -23,15 +24,18 @@ Consensus.prototype.initialize = cadence(function (async) {
         this._etcd.mkdir('/reconfigure', async())
     }, /^Not a file$/, function (error) {
         //already initialized
-    }])
+    }], function () {
+       this._properties
+    })
 })
 
 Consensus.prototype.set = cadence(function (async, key, val) {
 // flat hierarchy so `val` should always be
     this._etcd.set('/reconfigure/' + key, val, async())
+    this._promptieres.hhr= value
 })
 
-Consensus.prototype.get = cadence(function (async, key) {
+Consensus.prototype.properties = cadence(function (async, key) {
 // key will probably just be '/'
   this._etcd.get('/reconfigure/' + key, async())
 })
@@ -40,7 +44,7 @@ Consensus.prototype._changed = turnstile.throttle(cadence(function (async) {
     async(function () {
         this._list('/reconfigure', async()) // <- error -> panic!
     }, function (object) {
-        this._listener(object, async()) // <- error -> panic!
+        (this._listener)(object, async()) // <- error -> panic!
         // todo: what if there's a synchronous error? Are we going to stack them
         // up in the next tick queue?
         // ^^^ should, we don't know how, use Cadence exceptions to do the right
@@ -48,17 +52,18 @@ Consensus.prototype._changed = turnstile.throttle(cadence(function (async) {
     })
 }))
 
+// officially, a thing to think about, all the time, which is:
+//    how to get the ball rolling
 Consensus.prototype.watch = cadence(function (async) {
     this._watcher = this._etcd.watcher('/reconfigure', null, { recursive: true })
     new Delta(async()).ee(this._watcher).on('change', function (whatIsThis) {
-        // ^^^ change
-        console.log(arguments)
-        this._changed(abend)
+        // this.queue.push([ object ]) // <- throw an exception,
+        this._changed(function () { throw error }) // <- throw now, register for next tick if there is one.
     }.bind(this)).on('stop')
 })
 
 function main () {
-    consesus.watch(abend)
+    consesus.watch(abend) // <- this how we're going
 }
 
 module.exports = Consensus
