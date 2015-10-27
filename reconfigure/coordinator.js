512c1640c30f423602571acfbc9f077a42ecd24b
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-27 13:59:25 -0400

    Coordinator/Consensus cleanup.

--- reconfigure/coordinator.js
+++ reconfigure/coordinator.js
@@ -19,26 +19,30 @@ Coordinator.prototype.unlisten = cadence(function (async, url) {
     this._consensus.removeListener(url, async())
 })
 
-/*
-Coordinator.prototype.update = cadence(function (async) {
-    async.forEach(function (urls) {
-        async(function () {
-            // http POST and service is missing
-        }, function (body, response) {
-            if (!response.okay) {
-                setTimeout(function () { }, 60000)
-            }
-        }, function () {
-        })
-    })(this._listeners)
+Coordinator.prototype.list = cadence(function (async) {
+    this._consensus.list(async())
 })
 
-*/
+Coordinator.prototype.update = cadence(function (async) {
+    async(function () {
+        this._consensus.listeners(async())
+    }, function (list) {
+        async.forEach(function (urls) {
+            /*
+            async(function () {
+                // http POST and service is missing
+            }, function (body, response) {
+                if (!response.okay) {
+                    setTimeout(function () { }, 60000)
+                }
+            }, function () {
+            })
+                */
+        })(list)
+    })
+})
+
+module.exports = Coordinator
 /* Coordinator.prototype.set = cadence(function (async) {
     function (callback) { self.update(callback) }
 })
-
-Coordinator.prototype.list = cadence(function (async) {
-}) */
-
-module.exports = Coordinator
