5884ca02a76a8946b6188c3ffc92e9ccccb2f0dd
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-29 14:32:15 -0400

    Start server.

--- bin.js
+++ bin.js
@@ -19,7 +19,8 @@ require('arguable')(module, require('cadence')(function (async, options) {
     var UserAgent = require('./reconfigure/ua.js')
     var Consensus = require('./reconfigure/consensus.js')
     var Coordinator = require('./reconfigure/coordinator.js')
-    var http = require ('./reconfigure/http.js')
+    var service = require ('./reconfigure/http.js')
+    var http = require('http')
     switch (options.command[0]) {
         case 'serve':
             console.log('coming soon')
@@ -45,14 +46,14 @@ require('arguable')(module, require('cadence')(function (async, options) {
             ip = ip[0]
         }
         async(function () {
-            var server = new http({
+            var server = new service({
                 coordinator: new Coordinator(
                     new Consensus('reconfigure', ip, port, async()),
                     new UserAgent()
                 )
             })
-            var disp = server.dispatcher()
-            disp.server()
+            server = http.createServer(server.dispatcher().server())
+            server.listen(port, ip)
         }, function () {
             //Consensus stopped
         })
