8a61952c11ee1ddb59dc8d61fbcf8b96c10a6054
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-29 14:15:53 -0400

    Create an HTTP with a coordinator.

--- bin.js
+++ bin.js
@@ -16,20 +16,16 @@
 */
 
 require('arguable')(module, require('cadence')(function (async, options) {
+    var UserAgent = require('./reconfigure/ua.js')
+    var Consensus = require('./reconfigure/consensus.js')
+    var Coordinator = require('./reconfigure/coordinator.js')
+    var http = require ('./reconfigure/http.js')
     switch (options.command[0]) {
         case 'serve':
-            var ip, port
             console.log('coming soon')
-            ip = options.param.listen.split(':')
-            if (ip.length) {
-                port = ip[1]
-                ip = ip[0]
-            } else {
-                port = '8080'
-                ip = ip[0]
-            }
-            console.log('ip: ' + ip + ' port: ' + port)
+            serve(options.param.listen.split(':'))
             break
+
         case 'greeting':
             if (options.param.string != null) {
                 console.log(options.param.string)
@@ -37,5 +33,28 @@ require('arguable')(module, require('cadence')(function (async, options) {
                 console.log('Hello, World!')
             }
             break
+    }
+
+    function serve (ip) {
+        var port
+        if (ip.length) {
+            port = ip[1]
+            ip = ip[0]
+        } else {
+            port = '8080'
+            ip = ip[0]
+        }
+        async(function () {
+            var server = new http({
+                coordinator: new Coordinator(
+                    new Consensus('reconfigure', ip, port, async()),
+                    new UserAgent()
+                )
+            })
+            var disp = server.dispatcher()
+            disp.server()
+        }, function () {
+            //Consensus stopped
+        })
     }
 }))
