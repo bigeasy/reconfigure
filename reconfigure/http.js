7532d969bb48f58541795b24159aefc41a977254
Demarius Chrite <chrite.demarius@gmail.com>
2015-10-30 22:43:19 -0400

    Building out `http.js`.

--- reconfigure/http.js
+++ reconfigure/http.js
@@ -1,8 +1,15 @@
 var cadence = require('cadence')
 var Dispatcher = require('inlet/dispatcher')
 var logger = require('prolific').createLogger('diverter.allocator')
+var Consensus = require('./consensus')
+var Coordinator = require('./coordinator')
+var useragent = require('./ua')
 
-function Reconfigure () {
+function Reconfigure (key, host, port, listener) {
+    this._coordinator = new Coordinator(
+        new Consensus(key, host, port, listener), // uninitialized
+        new useragent()
+    )
 }
 
 Reconfigure.prototype.dispatcher = function (options) {
@@ -19,6 +26,21 @@ Reconfigure.prototype.dispatcher = function (options) {
 
 Reconfigure.prototype.index = cadence(function (async) {
     return 'Reconfigure API'
+})
+
+Reconfigure.prototype.register = cadence(function (async, listener) {
+    this._coordinator.listen(listener, async())
+})
+
+Reconfigure.prototype.deregister = cadence(function (async, listener) {
+    this._coordinator.unlisten(listener, async())
+})
+
+Reconfigure.prototype.set = cadence(function (async, key, value) {
+    this._coordinator.set(key, value, async())
+})
+
+Reconfigure.prototype.list = cadence(function (async) {
 })
 
 module.exports = Reconfigure
