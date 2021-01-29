cd14909c96398537e88eca64b3918e2b9e982944
Alan Gutierrez <alan@prettyrobots.com>
2021-01-29 10:20:50 -0600

    Remove Prospective dependency.

--- test/reconfigure.t.js
+++ test/reconfigure.t.js
@@ -1,6 +1,5 @@
 require('proof')(5, async (okay) => {
     const path = require('path')
-    const callback = require('prospective/callback')
     const fs = require('fs').promises
     const Reconfigurator = require('..')
     const file = path.join(__dirname, 'configuration.json')
@@ -31,22 +30,22 @@ require('proof')(5, async (okay) => {
                 test.push(configuration)
             }
         })()
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         await fs.writeFile(path.join(__dirname, 'configuration.foo'), '{ "x": 1 }')
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         await fs.writeFile(file, '{ "x": 1 }')
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         okay(test.splice(0), [{ method: 'configure', body: { x: 1 } }], 'load')
         await fs.writeFile(file, '{ "x": ')
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         // OS X generates one error, Linux two, so let's see that we get at
         // least one.
         okay(test.splice(0)[0].body.causes[0] instanceof SyntaxError, 'error')
         await fs.writeFile(file, '{ "x": 2 }')
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         reconfigurator.unshift(Buffer.from(JSON.stringify({ x: 2 })))
         okay(test.splice(0), [{ method: 'configure', body: { x: 2 } }], 'reconfigure')
-        await callback(callback => setTimeout(callback, 100))
+        await new Promise(resolve => setTimeout(resolve, 100))
         reconfigurator.destroy()
         reconfigurator.destroy()
         okay(test.splice(0), [{ method: 'configure', body: { x: 2 } }], 'unshift')
